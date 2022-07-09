2cf15fe49ff9ee225f9674cbcf60ee69e8ef751e
Alissa-1 <alena.cheremukhina@gmail.com>
2022-07-09 19:24:00 +0300

    Task 2: AlreadyExistsException

--- src/test/java/ru/netology/domain/ProductRepositoryTest.java
+++ src/test/java/ru/netology/domain/ProductRepositoryTest.java
@@ -12,6 +12,8 @@ public class ProductRepositoryTest {
     Product item1 = new Book(1, "nameqwerty1", 60, "author1");
     Product item2 = new Book(2, "name2", 70, "author2");
     Product item3 = new Smartphone(3, "name3", 80, "manuf3");
+    Product item4 = new Smartphone(4, "name4", 90, "manuf4");
+    Product item5 = new Smartphone(1, "nameqwerty1", 60, "author1");
 
     @BeforeEach
     public void setup() {
@@ -35,6 +37,24 @@ public class ProductRepositoryTest {
     public void shouldReturnMassageIfIdDoesntExist() {
         Assertions.assertThrows(NotFoundException.class, ()-> {
             repo.removeById(115);
+        });
+    }
+
+    @Test
+    public void shouldAddOneProductIfUniqueId() {
+        repo.add(item4);
+
+        Product[] expected = {item1, item2, item3, item4};
+        Product[] actual = repo.findAll();
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldReturnMassageIfIdAlreadyExistWhenAdd() {
+
+        Assertions.assertThrows(AlreadyExistsException.class, ()-> {
+            repo.add(item5);
         });
     }
 }
